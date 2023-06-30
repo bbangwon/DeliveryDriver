701cfe3206c568d9a48c68b12962366a6cd51233
youngil <youngil@placeb.com>
2023-06-30 09:27:11 +0900

    SerializeField

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    float steerSpeed = 1f;
-    float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+
     void Start()
     {
         
