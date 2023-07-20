d45303fe5ff8d7861c7bbd7ed37bba4e39b94eb1
youngil <youngil@placeb.com>
2023-07-20 09:51:22 +0900

    Pacakge 배달

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+
+    [SerializeField]
+    float destroyDelay = 0.5f;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Ouch!");
@@ -9,14 +14,17 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Package")
+        if(collision.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up");
+            hasPackage = true;
+            Destroy(collision.gameObject, destroyDelay);
         }
 
-        if(collision.tag == "Customer")
+        if(collision.tag == "Customer" && hasPackage)
         {
             Debug.Log("Delivered package");
+            hasPackage = false;
         }
     }
 }
