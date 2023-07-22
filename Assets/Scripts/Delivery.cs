91dabfd2686ea9ac432815bd227e4d4482174dbf
youngil <youngil@placeb.com>
2023-07-22 16:09:45 +0900

    패키지 컬러 변경

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
     bool hasPackage;
 
     [SerializeField]
     float destroyDelay = 0.5f;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -18,12 +29,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(collision.gameObject, destroyDelay);
         }
 
         if(collision.tag == "Customer" && hasPackage)
         {
             Debug.Log("Delivered package");
+            spriteRenderer.color = noPackageColor;
             hasPackage = false;
         }
     }
